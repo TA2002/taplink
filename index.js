e5011a054f8d3b72a4977ae4cbd4b48b14ee64c7
Tarlan Askaruly <as.tarlan02@gmail.com>
2023-12-04 21:23:30 -0500

    cors

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const connectDB = require("./db");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
+
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(cors());
 
 // Parse JSON request body
 app.use(express.json());
